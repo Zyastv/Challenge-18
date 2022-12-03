4034340f0da64cf0dd880ced188d0308b450667f
Zachary Adams <Zach2vegas@gmail.com>
2022-12-02 19:15:48 -0800

    commit

---
+++ models/user.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create user model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      validate: [validateEmail, 'Please use a valid email address'],
+    },
+    thoughts: {
+      type: String,
+      required: true,
+      max_length: 50,
+    },
+    assignments: [assignmentSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+const User = model('user', userSchema);
+
+module.exports = User;
